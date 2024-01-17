775edaec62f2ffcd900c106580b29d74502d7a81
Denis Knor <knorred@mail.ru>
2024-01-17 23:00:55 +0300

    feat: validation to routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,32 @@
 const { createCard, getCards, deleteCardById, likeCard, dislikeCard } = require('../controllers/cards');
 const Router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
-Router.post('/cards', createCard);
+Router.post('/cards', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.link().required(),
+    owner: Joi.string(),
+  }),
+}), createCard);
 Router.get('/cards', getCards);
-Router.delete('/cards/:cardId', deleteCardById);
-Router.put('/cards/:cardId/likes', likeCard);
-Router.delete('/cards/:cardId/likes', dislikeCard);
+Router.delete('/cards/:cardId', celebrate({
+  body: Joi.object().keys({
+    cardId: Joi.string(),
+  }),
+}), deleteCardById);
+Router.put('/cards/:cardId/likes', celebrate({
+  body: Joi.object().keys({
+    cardId: Joi.string(),
+  }),
+}), likeCard);
+Router.delete('/cards/:cardId/likes',
+celebrate({
+  body: Joi.object().keys({
+    cardId: Joi.string(),
+  }),
+}), dislikeCard);
 
 module.exports = Router;
+
+
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,40 @@ const { createUser, getUsers, getUserById, updateUserById, updateUserAvatarById,
 const Router = require('express').Router();
 const auth = require('../middlewares/auth');
 const errorHandler = require('../errors/error-handler');
+const { celebrate, Joi } = require('celebrate');
+//name, about, avatar, email, password
 
-
-Router.post('/signin', login);
-Router.post('/signup', createUser);
+Router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+Router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.link().required(),
+    avatar: Joi.string().required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 Router.use(auth);
 Router.get('/users', getUsers);
-Router.get('/users/:userId', getUserById);
-Router.patch('/users/me', updateUserById);
-Router.patch('/users/me/avatar', updateUserAvatarById);
+Router.get('/users/:userId', celebrate({
+  body: Joi.object().keys({
+    userId: Joi.string().required(),
+  }),
+}), getUserById);
+Router.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    userId: Joi.string().required(),
+  }),
+}), updateUserById);
+Router.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    userId: Joi.string().required(),
+  }),
+}), updateUserAvatarById);
 Router.use(errorHandler);
 module.exports = Router;
