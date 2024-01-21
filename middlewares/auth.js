adce4075b59fa30f985488aa543e26066fa19ea5
Denis Knor <knorred@mail.ru>
2024-01-21 18:14:29 +0300

    fix: autorization jwt token write in req.user

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,18 @@
 const { isAuthorized } = require("../utils/jwt");
-const UnauthorizedError =require('../errors/unauthorized-error');
+const UnauthorizedError = require('../errors/unauthorized-error');
+const { JWT_SECRET = "SECRET_KEY" } = process.env;
 
 const auth = (req, res, next) => {
   const token = req.headers.authorization;
- if (!isAuthorized(token)) {next(new UnauthorizedError('not autorization'));}//return res.status(401).send({ message: 'not autorization' });
-  next();
+  if (!isAuthorized(token)) { next(new UnauthorizedError('not autorization')); }//return res.status(401).send({ message: 'not autorization' });
+  let payload
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (e) {
+    return next(new UnauthorizedError('not autorization'));
+  }
+  req.user = payload;
+  return next();
 };
 
 module.exports = auth;
