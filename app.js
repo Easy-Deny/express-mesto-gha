27f184948810b8c639fdcd0cd1798004c2ea3e39
Denis Knor <knorred@mail.ru>
2024-01-21 18:03:30 +0300

    fix: add custom error to app.all(*)

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./errors/error-handler');
 const { celebrate, Joi, errors } = require('celebrate');
-
+const NotFoundError = require('./errors/not-found-error');
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 })
@@ -23,11 +23,11 @@ app.use(express.json());
 app.use(userRouter);
 app.use(cardRouter);
 app.use(errors());
-app.all('*', (req, res) => {
-  res.status(404).send({ message: '404! Page not found' });
+app.all('*', (req, res, next) => {
+  throw new NotFoundError('404! Page not found');
+  //res.status(404).send({ message: '404! Page not found' });
 });
 app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`port ${PORT}`);
 });
-
