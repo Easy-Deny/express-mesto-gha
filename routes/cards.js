9c55c08aae6ae528103fb468131bab37d2f52a72
Denis Knor <knorred@mail.ru>
2024-01-23 18:49:11 +0300

    fix. reviewer comment fixed v2

--- routes/cards.js
+++ routes/cards.js
@@ -11,7 +11,7 @@ const {
 Router.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(/^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/),
+    link: Joi.string().required().pattern(/^(https?:\/\/)?([\w.]+)\.([a-z]{2,6}\.?)(\/[\w.]*)*\/?$/),
   }),
 }), createCard);
 Router.get('/cards', getCards);
--- routes/users.js
+++ routes/users.js
@@ -21,27 +21,29 @@ Router.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required(),
-    avatar: Joi.string().required().pattern(/^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/),
+    avatar: Joi.string().required().pattern(/^(https?:\/\/)?([\w.]+)\.([a-z]{2,6}\.?)(\/[\w.]*)*\/?$/),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), createUser);
 Router.use(auth);
+Router.get('/users/me', updateUser);
 Router.get('/users/:userId', celebrate({
   body: Joi.object().keys({
     userId: Joi.string().length(24).hex().required(),
   }),
 }), getUserById);
-Router.get('/users/me', updateUser);
 Router.get('/users', getUsers);
 Router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     userId: Joi.string().length(24).hex().required(),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required(),
   }),
 }), updateUserById);
 Router.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required().pattern(/^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/),
+    avatar: Joi.string().required().pattern(/^(https?:\/\/)?([\w.]+)\.([a-z]{2,6}\.?)(\/[\w.]*)*\/?$/),
   }),
 }), updateUserAvatarById);
 module.exports = Router;
